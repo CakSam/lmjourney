6a415d51135fcd452b41baad4018e4057699a918
CakSam <40931489+CakSam@users.noreply.github.com>
2018-07-09 16:50:15 +0700

    slide 5

--- js/auto-slide.js
+++ js/auto-slide.js
@@ -3,7 +3,7 @@ window.slide = 1
 function nextSlide(){
    //increase window.slide by 1 (to go to the next slide), 
    //or if it is 3 return to the first slide
-   window.slide == 4 ? window.slide = 1 : window.slide++;
+   window.slide == 5 ? window.slide = 1 : window.slide++;
    //trigger click event on the NEXT slide only
    $('a[href=#slide'+window.slide+']').click()
 }
